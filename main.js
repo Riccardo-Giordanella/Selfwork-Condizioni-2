8f4552e15316e6beeba0c61c5d6541e4f2bf5a7e
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-04 23:41:53 +0200

    Switch fixato

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Esercizio eseguito con if/else (Lascio commentato per non interferire con lo switch)
 
-let temperatura = prompt(`Inserisci la temperatura attuale:`);
+let temperatura = Number(prompt(`Inserisci la temperatura attuale:`));
+
 
 // if(temperatura < 20 && temperatura >= 0){
 //     console.log(`non ci sono più le mezze stagioni`);
@@ -21,60 +22,23 @@ let temperatura = prompt(`Inserisci la temperatura attuale:`);
 
 // Inserimento di switch
 switch(temperatura){
-    case `-11`:
+    case temperatura < -10:
         console.log(`copriti…ancora ti raffreddi`);
     break;
 
-    case `-10`:
-    case `-9`:
-    case `-8`:
-    case `-7`:
-    case `-6`:
-    case `-5`:
-    case `-4`:
-    case `-3`:
-    case `-2`:
-    case `-1`:
+    case temperatura < 0:
         console.log(`non è tanto il freddo quanto l’umidità`);
     break;
 
-    case `0`:
-    case `1`:
-    case `2`:
-    case `3`:
-    case `4`:
-    case `5`:
-    case `6`:
-    case `7`:
-    case `8`:
-    case `9`:
-    case `10`:
-    case `11`:
-    case `12`:
-    case `13`:
-    case `14`:
-    case `15`:
-    case `16`:
-    case `17`:
-    case `18`:
-    case `19`:
+    case temperatura < 20:
         console.log(`non ci sono più le mezze stagioni`);
         break;
 
-    case `20`:
-    case `21`:
-    case `22`:
-    case `23`:
-    case `24`:
-    case `25`:
-    case `26`:
-    case `27`:
-    case `28`:
-    case `29`:
+    case temperatura < 30:
         console.log(`mi dia una peroni sudata`);
     break;
 
-    case `30`:
+    default:
         console.log(`lu mare, lu sole, lu ientu`);
     break;
 }
